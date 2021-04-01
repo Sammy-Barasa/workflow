12e0d409594d283318c7f4edc921c8ae8536d096
Sammy-Barasa <kesammybarasa@gmail.com>
2021-04-01 18:58:05 +0300

    change to material ui IconButtons

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,12 +1,17 @@
 import React,{useState,useContext} from 'react'
-import { List,Icon,Button } from 'semantic-ui-react'
+import { List,Icon } from 'semantic-ui-react'
 import StateContext from '../Context/stateContext'
 import SingleWorkItem from "./SingleWorkItem"
 import Input from '@material-ui/core/Input'
+import {useHistory} from 'react-router-dom'
+import IconButton from '@material-ui/core/IconButton'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 import '../App.css'
 
 const Search = () => {
 
+    const history = useHistory()
     const { state } = useContext(StateContext)
     const work= state.work.data.data
     const [listToDisplay,setListToDisplay]=useState(work)
@@ -42,14 +47,32 @@ const Search = () => {
 
     return (
         <div className="search-page">
-            <div>
-                <h4>{result} results</h4>  
+            <div className="search-container">
+                <div className="search-input-back-button">
+                    
+                <IconButton color="default" aria-label="back button" component="span" onClick={(e)=>{
+                    e.preventDefault()
+                    history.goBack()}}>
+                    <ArrowBackIosIcon />
+                </IconButton>
+                </div>
+                <div className="search-input">
+                    <Input type="search" id="search" name="search" placeholder="Search here ... by topic or order number ..." 
+                    value={query} onChange={onchange} autoFocus fullWidth={true}/>
+                </div>
             </div>
-                <Input type="search" id="search" name="search" placeholder="Search here ..." value={query} onChange={onchange} autoFocus/>
+            
+                
             {scope.hasNolist?<h4>Add persons first</h4>:<div className="sort">
                 <Icon name="filter" size="large">Sort:</Icon>
                 { persons.map((person)=>{
-                    return <Button className='sort-button' name={person.name} onClick={sortByPerson}><Icon name="user circle outline" size="small">{person.name }</Icon></Button>
+                    return (
+                    <IconButton className='sort-button' name={person.name} color="default" aria-label="back button" component="span" onClick={sortByPerson}>
+                        <AccountCircleIcon/>
+                        {person.name }
+                    </IconButton>
+                    )
+                    
                 })}
 
                 {sortOnProgress?<Icon id="cancel-sort"name="cancel" size="big" color='red' onClick={(e)=>{
@@ -58,6 +81,9 @@ const Search = () => {
                     setSortOnProgress(false)
                 }}></Icon>:""}
             </div>}
+            <div>
+                <h4>{result} results</h4>  
+            </div>
             <div className='worklist-container'>    
                 <List divided relaxed> 
                     {                                                                                                                                                                               
--- src/Components/UserWorkList.js
+++ src/Components/UserWorkList.js
@@ -1,12 +1,14 @@
 import React, { useContext,useEffect } from 'react'
 // import { Button } from 'semantic-ui-react'
 import StateContext from '../Context/stateContext'
-import { List,Icon } from 'semantic-ui-react'
+import { List } from 'semantic-ui-react'
 import SingleWorkItem from "./SingleWorkItem"
 import FloatingActionButton from "./FloatingActionButton"
 import ListSkeleton from "../Utils/ListSkeleton"
 import EmptyList from "../Utils/EmptyList"
 import {useHistory} from 'react-router-dom'
+import IconButton from '@material-ui/core/IconButton'
+import SearchIcon from '@material-ui/icons/Search';
 import '../App.css'
 
 
@@ -21,22 +23,18 @@ const UserWorkList = () => {
     const Loading = state.workupdate.loading
     const history = useHistory()
     useEffect(()=>{},[loading,Loading]) 
-    // const workderror=work?.error
-    // const searchChange=async (query)=>{
-        // let filtered = work.filter((sinlework)=>{
-        // return sinlework.topic.toLowerCase().includes(query.toLowerCase())||sinlework.person.toLowerCase().includes(query.toLowerCase())
-        // })
-        // setQuery(query)
-        // console.log(filtered)
-    // }
+    
     return (
                 <div >
                     <div className='user-worklist-header'>
                         <h3>My List of Works</h3>
-                    <Icon name="search" size="large" onClick={(e)=>{
+                    
+                    <IconButton color="default" aria-label="go to search page" component="span" onClick={(e)=>{
                         e.preventDefault()
-                        history.push(`/users/${username}/search`)
-                    }}/>
+                        history.push(`/users/${username}/search`)}}>
+                        <SearchIcon />
+                    </IconButton>
+
                     </div>
                     
                         {   
